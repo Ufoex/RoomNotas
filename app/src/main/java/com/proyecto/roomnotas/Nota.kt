252f899f2ab78c35f2d8013d6f808e7f49dffbf6
Ufoex <leonardo.ulloa@alumno.udg.mx>
2020-12-15 23:28:39 -0600

    Proyecto subido

---
+++ app/src/main/java/com/proyecto/roomnotas/Nota.kt
@@ -0,0 +1,12 @@
+package com.proyecto.roomnotas
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Nota(
+    @PrimaryKey(autoGenerate = true)
+    val id:Int,
+    val titulo:String,
+    val contenido:String
+)
---
+++ app/src/main/java/com/proyecto/roomnotas/NotaApp.kt
@@ -0,0 +1,19 @@
+package com.proyecto.roomnotas
+
+import android.app.Application
+import androidx.room.Room
+
+class NotaApp: Application() {
+
+
+    lateinit var baseDeDatos:NotasDb
+
+
+    override fun onCreate() {
+        super.onCreate()
+        baseDeDatos = Room
+            .databaseBuilder(this,NotasDb::class.java,"notas_db")
+            .build()
+    }
+
+}
